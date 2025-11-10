259aa125d3268a6e8eb06688b71edf36e008fcb3
bbanggang <hungue6559@naver.com>
2025-11-10 15:38:20 +0900

    25_11_10

---
+++ 25_11_10/test2/complex.cpp
@@ -0,0 +1,20 @@
+#include <iostream> // 표준 입출력 스트림 사용
+#include "complex.hpp" // 정의된 Complex 클래스를 포함하는 헤더 파일 포함
+using namespace std; // std 네임스페이스 사용
+
+Complex::Complex() : real(0), img(0) {} // 기본 생성자 정의
+Complex::Complex(int r, int i) : real(r), img(i) {
+    cout << "복소수 " << r << " + " << i << "j" << " 생성" << endl; // 복소수 생성 메시지 출력
+} // 값 생성자 정의
+Complex& operator++(Complex& x) { // 전위 증가 연산자 오버로딩 함수 정의
+    x.real++; // 실수부 증가
+    x.img++; // 허수부 증가
+    return x; // x를 반환
+}
+Complex operator--(Complex& x, int) { // 후위 감소 연산자 오버로딩 함수 정의
+    Complex temp = x; // x를 임시 객체에 저장
+    x.real--; // 실수부 감소
+    x.img--; // 허수부 감소
+    return temp; // temp를 반환
+}
+void Complex::show() { cout << real << " + " << img << "j" << endl; } // 현재 값을 출력
---
+++ 25_11_10/test3/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream> // 표준 입출력 스트림 사용을 위한 헤더 포함
+#include "power.hpp" // Power 클래스 선언을 포함
+using namespace std; // std 네임스페이스를 사용
+
+int main() { // main 함수의 머리
+    Power a(3,5), b; // a는 (3,5)로 초기화, b는 (0,0) 초기화
+    a.show(); // a의 멤버 변수 출력
+    b.show(); // b의 멤버 변수 출력
+    b = 2*a; // a의 두 멤버 변수에 2를 곱한 새 객체를 b에 대입
+    a.show(); // a는 변하지 않음
+    b.show(); // b의 멤버 변수 출력
+    return 0; // 0을 반환하고 함수 종료
+}   // main 함수의 끝
+
+
